8bacb4b78e231b199925111380abda832c1a5a17
KimHuiMang <piyrw9754@gmail.com>
2022-11-16 09:29:28 +0900

    [2292] 벌지

--- src/b_2292/Main.java
+++ src/b_2292/Main.java
@@ -1,2 +1,20 @@
-package b_2292;public class Main {
+package b_2292;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        a--;
+        int count=1;
+
+        while(a>0){
+            a-=count*6;
+            count++;
+        }
+
+        System.out.println(count);
+    }
 }
