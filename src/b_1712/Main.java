e4f7d185fbe3055465ece766e9bbed0bb5cd09c1
KimHuiMang <piyrw9754@gmail.com>
2022-11-16 09:25:31 +0900

    [1712] 벌집

--- src/b_1712/Main.java
+++ src/b_1712/Main.java
@@ -5,17 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
-
-            int A = in.nextInt();	// 불변 비용
-            int B = in.nextInt();	// 가변 비용
-            int C = in.nextInt(); 	// 상품 가격
-
-            if (C <= B) {
-                System.out.println("-1");
+            int input = in.nextInt();
+            int count = 1;
+            input--;
+            while(input > 0) {
+                input -= count * 6;
+                count++;
             }
-            else {
-                System.out.println((A/(C-B))+1);
-            }
+            System.out.println(count);
     }
 }
 
