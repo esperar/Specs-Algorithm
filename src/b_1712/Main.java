4016939379901f2cf21b4adea134711b294f21a4
KimHuiMang <piyrw9754@gmail.com>
2022-11-16 09:29:02 +0900

    [1712] 손익 분기점

--- src/b_1712/Main.java
+++ src/b_1712/Main.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            int input = in.nextInt();
-            int count = 1;
-            input--;
-            while(input > 0) {
-                input -= count * 6;
-                count++;
-            }
-            System.out.println(count);
+        Scanner in = new Scanner(System.in);
+
+        int A = in.nextInt();    // 불변 비용
+        int B = in.nextInt();    // 가변 비용
+        int C = in.nextInt();     // 상품 가격
+
+        if (C <= B) {
+            System.out.println("-1");
+        }
+        else {
+            System.out.println((A/(C-B))+1);
+        }
     }
 }
 
---
+++ src/b_2292/Main.java
@@ -0,0 +1,2 @@
+package b_2292;public class Main {
+}
