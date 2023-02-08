e269469774667887742a54bae580f2b23eff40bf
aesperer <piyrw9754@gmail.com>
2023-02-08 21:45:50 +0900

    :sparkles: [5585] 거스름 돈
    거스름돈

---
+++ src/baekjoon/b_5585/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.b_5585;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int money = 1000;
+        int count = 0;
+        int N = sc.nextInt();
+        int[] m = {500 , 100, 50, 10, 5, 1};
+        money = money - N;
+
+        for(int i = 0; i < m.length; i++) {
+            if(money >= m[i]){
+                count += money / m[i];
+                money = money % m[i];
+            }
+
+            if(money == 0){
+                break;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
