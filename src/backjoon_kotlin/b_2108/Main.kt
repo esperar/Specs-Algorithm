373cbcd1baaea8e1d86cf7010a054369c23aa3f5
aesperer <piyrw9754@gmail.com>
2023-03-22 11:42:00 +0900

    2108 통계학

--- src/backjoon_kotlin/b_2108/Main.kt
+++ src/backjoon_kotlin/b_2108/Main.kt
@@ -13,7 +13,6 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
         arr.add(readLine().toInt())
     }
 
-
     arr.sort()
     // -1 -1 -2 -2 -3
     val average = (arr.sum() / N.toDouble()).roundToInt()
