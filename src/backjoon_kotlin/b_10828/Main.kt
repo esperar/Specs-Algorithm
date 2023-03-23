c0bc347a6f513976f2ec30e1508abf3b5ebed2cf
aesperer <piyrw9754@gmail.com>
2023-03-23 12:00:12 +0900

    10828 스택;

---
+++ src/backjoon_kotlin/b_10828/Main.kt
@@ -0,0 +1,19 @@
+package backjoon_kotlin.b_10828
+
+import java.util.*
+
+fun main() {
+    val stack = Stack<Int>()
+
+    val n = readLine()!!.toInt()
+    repeat(n) {
+        val command = readLine()!!.split(" ")
+        when (command[0]) {
+            "push" -> stack.push(command[1].toInt())
+            "pop" -> println(if (stack.isEmpty()) -1 else stack.pop())
+            "size" -> println(stack.size)
+            "empty" -> println(if (stack.isEmpty()) 1 else 0)
+            "top" -> println(if (stack.isEmpty()) -1 else stack.peek())
+        }
+    }
+}
