b9b5faa6182373f6a6eff10184106af191860845
aesperer <piyrw9754@gmail.com>
2023-04-14 22:23:57 +0900

    테스트 코드

--- src/study_note/tree/Tree.kt
+++ src/study_note/tree/Tree.kt
@@ -1,5 +1,7 @@
 package study_note.tree
 
+import java.util.Random
+
 class Node(var value: Int) {
     var left: Node? = null
     var right: Node? = null
@@ -128,10 +130,37 @@ class NodeMgmt(var head: Node) {
 }
 
 fun main() {
-    val head = Node(1)
+    val head = Node(500)
     val BST = NodeMgmt(head)
-    BST.insert(2)
-    BST.insert(3)
-    BST.insert(0)
-    println(BST.search(2))
+    val random = Random()
+    val insertSet = hashSetOf<Int>()
+
+    while(insertSet.size != 100){
+        insertSet.add(random.nextInt(101))
+    }
+
+    for(i in insertSet){
+        BST.insert(i)
+    }
+
+    for(num in insertSet){
+        if(!BST.search(num)){
+            print("searched Fail! $num")
+        }
+        println("$num")
+    }
+
+    val deleteSet = hashSetOf<Int>()
+    val iterator = insertSet.iterator()
+    for(i in 0 until 10){
+        if(iterator.hasNext()){
+            deleteSet.add(iterator.next())
+        }
+    }
+
+    for(deleteNum in deleteSet){
+        if(!BST.search(deleteNum))
+            println("searched faild $deleteNum")
+    }
+
 }
