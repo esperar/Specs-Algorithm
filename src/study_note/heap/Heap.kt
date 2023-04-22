3e5771537ebd6af8f2380a1331516e58b14b6084
aesperer <piyrw9754@gmail.com>
2023-04-22 22:13:48 +0900

    :art: :: pop

--- src/study_note/heap/Heap.kt
+++ src/study_note/heap/Heap.kt
@@ -23,4 +23,62 @@ class Heap(private val value: Int) {
             insertIdx = parentIdx
         }
     }
+
+    fun moveDown(popIdx: Int): Boolean{
+        val leftChildNodeIdx = popIdx * 2
+        val rightChildNodeIdx = popIdx * 2 + 1
+
+        // 왼쪽 자식 노드도 없을 때
+        if(leftChildNodeIdx >= heapList.size) {
+            return false
+        }
+
+        // 왼쪽 자식 노드만 있을 때
+        else if(rightChildNodeIdx >= heapList.size){
+            return heapList[popIdx]!! <= heapList[leftChildNodeIdx]!!
+        }
+
+        // 왼쪽, 오른쪽 자식 모두 있을 때
+        else{
+            return if(heapList[leftChildNodeIdx]!! > heapList[rightChildNodeIdx]!!){
+                heapList[popIdx]!! <= heapList[leftChildNodeIdx]!!
+            } else {
+                heapList[popIdx]!! <= heapList[rightChildNodeIdx]!!
+            }
+        }
+    }
+
+    fun pop(): Int?{
+        if(heapList.size <= 1){
+            return null
+        }
+
+        val returnedData = heapList[1]
+        heapList[1] = heapList[heapList.size - 1]
+        heapList.removeLast()
+        var popIdx = 1
+
+        while(moveDown(popIdx)){
+            val leftChildNodeIdx = popIdx * 2
+            val rightChildNodeIdx = popIdx * 2 + 1
+
+            if(rightChildNodeIdx >= heapList.size){
+                heapList[popIdx].also { heapList[leftChildNodeIdx] = heapList[popIdx] }
+                popIdx = leftChildNodeIdx
+            }
+            else{
+                if(heapList[leftChildNodeIdx]!! > heapList[rightChildNodeIdx]!!){
+                    if(heapList[popIdx]!! < heapList[leftChildNodeIdx]!!){
+                        heapList[popIdx].also { heapList[leftChildNodeIdx] = heapList[popIdx] }
+                        popIdx = leftChildNodeIdx
+                    } else {
+                        heapList[popIdx].also { heapList[rightChildNodeIdx] = heapList[popIdx] }
+                        popIdx = rightChildNodeIdx
+                    }
+                }
+            }
+        }
+
+        return returnedData
+    }
 }
