f659259362ae4f05339d589c008565da43d4578d
aesperer <piyrw9754@gmail.com>
2023-04-29 12:35:18 +0900

    삽입 정렬

---
+++ src/study_note/sort/Insert.kt
@@ -0,0 +1,19 @@
+package study_note.sort
+
+fun insertSort(data: MutableList<Int>): MutableList<Int>{
+    for(i in 1 until data.size){
+        var index = i
+        val temp = data[i]
+        while (index > 0 && data[index-1] > temp){
+            data[index] = data[index-1]
+            index--
+        }
+        data[index]= temp
+    }
+    return data
+}
+
+fun main() {
+    val data = mutableListOf(2,6,8,4,3,9)
+    println(insertSort(data).toString())
+}
