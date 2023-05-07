e2d91fc27df8c7339928949d0d66283056cef418
aesperer <piyrw9754@gmail.com>
2023-05-07 12:05:43 +0900

    fibo recursive

--- src/study_note/recursive_call/factorial.kt
+++ src/study_note/recursive_call/factorial.kt
@@ -7,6 +7,19 @@ fun factorial(num: Int): Int =
         num * factorial(num - 1)
     }
 
+fun fibo(num: Int): Int {
+    if (num == 1) {
+        return num
+    }
+
+    return if (num % 2 == 1) {
+        fibo((3 * num) + 1)
+    } else {
+        fibo(num / 2)
+    }
+}
+
 fun main(){
     println(factorial(5))
+    println(fibo(5))
 }
