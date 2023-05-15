335ddbb4eaf566120734d8888103206301a358da
aesperer <piyrw9754@gmail.com>
2023-05-15 20:46:31 +0900

    1012 유기농 배추

---
+++ src/backjoon_kotlin/b_1012/Main.kt
@@ -0,0 +1,54 @@
+package backjoon_kotlin.b_1012
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val dx = arrayOf(0, 0, 1, -1)
+val dy = arrayOf(1, -1, 0, 0)
+
+lateinit var map: Array<Array<Int>>
+var n = 0
+var m = 0
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val T = br.readLine().toInt()
+
+    repeat(T) {
+        val mnk = br.readLine().split(" ")
+        m = mnk[0].toInt()
+        n = mnk[1].toInt()
+        val k = mnk[2].toInt()
+        var cnt = 0
+
+        map = Array(n) { Array(m) { 0 } }
+
+        repeat(k) {
+            val xy = br.readLine().split(" ")
+            map[xy[1].toInt()][xy[0].toInt()] = 1
+        }
+
+        repeat(n) { x ->
+            repeat(m) { y ->
+                if(dfs(x,y)) cnt++
+            }
+        }
+
+        println(cnt)
+    }
+}
+
+fun dfs(x: Int, y: Int): Boolean {
+    if (map[x][y] == 0) return false
+
+    map[x][y] = 0
+    for (i in 0 until 4) {
+        val nx = x + dx[i]
+        val ny = y + dy[i]
+
+        if (nx !in 0 until n || ny !in 0 until m || map[nx][ny] == 0)
+            continue
+        dfs(nx, ny)
+    }
+    return true
+}
