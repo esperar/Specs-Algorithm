457508f8865ce200a84518de150f9fb3018a6206
aesperer <piyrw9754@gmail.com>
2023-05-26 11:25:09 +0900

    11727 2xn 타일링 2

--- src/backjoon_kotlin/b_11727/Main.kt
+++ src/backjoon_kotlin/b_11727/Main.kt
@@ -2,13 +2,12 @@ package backjoon_kotlin.b_11727
 
 fun main() = with(System.`in`.bufferedReader()){
     val n = readLine().toInt()
-    val dp = IntArray(n)
-    dp[0] = 1
-    dp[1] = 3
+    val dp = IntArray(n + 1)
+    dp[1] = 1
+    dp[2] = 3
 
-    for(i in 2 until n){
-        dp[i] = dp[i - 1] + 2 * dp[i - 2]
+    for(i in 3 .. n){
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007
     }
-
-    println(dp[n - 1] % 10007)
+    println(dp[n])
 }
