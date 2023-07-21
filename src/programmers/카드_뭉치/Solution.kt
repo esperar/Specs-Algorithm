61eb144ed91448c129f453fac474706075bf22d3
aesperer <piyrw9754@gmail.com>
2023-07-21 16:20:36 +0900

    카드 뭉치

---
+++ src/programmers/카드_뭉치/Solution.kt
@@ -0,0 +1,24 @@
+package programmers.카드_뭉치
+
+
+class Solution {
+
+    fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
+        var answer = "Yes"
+        var card1Idx = 0
+        var card2Idx = 0
+
+        for(i in goal) {
+            if(card1Idx < cards1.size && cards1[card1Idx] == i) {
+                card1Idx++
+            } else if(card2Idx < cards2.size && cards2[card2Idx] == i){
+                card2Idx++
+            } else {
+                answer = "No"
+                break
+            }
+        }
+
+        return answer
+    }
+}
