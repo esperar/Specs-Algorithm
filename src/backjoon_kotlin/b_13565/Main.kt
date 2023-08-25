83cc82d6ef1bcb6dbcd94d765cc58491ed5e20ce
aesperer <piyrw9754@gmail.com>
2023-08-25 15:51:44 +0900

    13565 침투

--- src/backjoon_kotlin/b_13565/Main.kt
+++ src/backjoon_kotlin/b_13565/Main.kt
@@ -2,47 +2,51 @@ package backjoon_kotlin.b_13565
 
 import java.util.*
 
-val dirXY = arrayOf(intArrayOf(1, 0), intArrayOf(-1, 0), intArrayOf(0, 1), intArrayOf(0, -1))
-var canFinish = false
-
-fun dfs(r: Int, c: Int, graph: Array<String>) {
-    graph[r] = graph[r].substring(0, c) + '2' + graph[r].substring(c + 1)
-
-    for (dir in dirXY) {
-        val nr = r + dir[0]
-        val nc = c + dir[1]
-
-        if (nr == graph.size) {
-            canFinish = true
-            return
-        }
-
-        if (nr < 0 || nc >= graph[0].length || nc < 0) continue
-        if (graph[nr][nc] != '0') continue
-
-        dfs(nr, nc, graph)
-    }
-}
+var n = 0
+var m = 0
+lateinit var graph: Array<IntArray>
+lateinit var visited: Array<BooleanArray>
+val pos = arrayOf(intArrayOf(0, 1), intArrayOf(0, -1), intArrayOf(-1, 0), intArrayOf(1, 0))
+var check = false
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    val n = scanner.nextInt()
-    val m = scanner.nextInt()
+    val sc = Scanner(System.`in`)
 
-    val graph = Array(n) { "" }
+    n = sc.nextInt()
+    m = sc.nextInt()
+
+    graph = Array(n) { IntArray(m) }
+    visited = Array(n) { BooleanArray(m) }
+
     for (i in 0 until n) {
-        graph[i] = scanner.next()
-    }
-
-    for (c in 0 until m) {
-        if (graph[0][c] == '0') {
-            dfs(0, c, graph)
-            if (canFinish) {
-                println("YES")
-                return
-            }
+        val str = sc.next()
+        for (j in 0 until m) {
+            graph[i][j] = str[j] - '0'
         }
     }
 
-    println("NO")
+    for (i in 0 until m) {
+        if (graph[0][i] == 0) {
+            dfs(0, i)
+        }
+    }
+
+    println(if (check) "YES" else "NO")
+}
+
+fun dfs(row: Int, col: Int) {
+    visited[row][col] = true
+    if (row == n - 1 && graph[row][col] == 0) {
+        check = true
+        return
+    }
+
+    for (i in pos.indices) {
+        val nr = row + pos[i][0]
+        val nc = col + pos[i][1]
+
+        if (nr >= 0 && nc >= 0 && nr < n && nc < m && !visited[nr][nc] && graph[nr][nc] == 0) {
+            dfs(nr, nc)
+        }
+    }
 }
