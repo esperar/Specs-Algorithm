a4002c59c4d35715b60aa35412b69ed9437a33f9
aesperer <piyrw9754@gmail.com>
2023-11-01 14:36:40 +0900

    insertion sort

--- src/study_note/sort/Insert.kt
+++ src/study_note/sort/Insert.kt
@@ -4,8 +4,8 @@ fun insertSort(data: MutableList<Int>): MutableList<Int>{
     for(i in 1 until data.size){
         var index = i
         val temp = data[i]
-        while (index > 0 && data[index-1] > temp){
-            data[index] = data[index-1]
+        while (index > 0 && data[index - 1] > temp){
+            data[index] = data[index - 1]
             index--
         }
         data[index]= temp
