b564151b0b244dbfdbc99682290fa11ac4541ed0
aesperer <piyrw9754@gmail.com>
2023-11-25 19:10:29 +0900

    20291 파일 정리

---
+++ src/backjoon_kotlin/b_20291/Main.kt
@@ -0,0 +1,22 @@
+package backjoon_kotlin.b_20291
+
+import java.lang.StringBuilder
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val map = mutableMapOf<String, Int>()
+    val sb = StringBuilder()
+
+    for(i in 0 until n) {
+        val (name, extension) = readLine().split(".")
+        map[extension] = map[extension]?.plus(1) ?: 1
+    }
+
+    val sortedMap = map.toList().sortedBy { it.first }.toMap()
+
+    for ((key, value) in sortedMap) {
+        sb.append("$key $value\n")
+    }
+
+    println(sb)
+}
