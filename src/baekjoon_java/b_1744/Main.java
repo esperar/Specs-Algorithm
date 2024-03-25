41bd890f6d057b34ef2b9c4b4a7354537ca2ac3a
aesperer <piyrw9754@gmail.com>
2024-03-25 14:14:35 +0900

    1744 수 묶기;

---
+++ src/baekjoon_java/b_1744/Main.java
@@ -0,0 +1,47 @@
+package baekjoon_java.b_1744;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        List<Integer> p = new ArrayList<>();
+        List<Integer> m = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            int input = sc.nextInt();
+            if(input <= 0) {
+                m.add(input);
+            } else {
+                p.add(input);
+            }
+        }
+
+        int sum = 0;
+
+        p.sort(Collections.reverseOrder());
+        Collections.sort(m);
+
+        int i = 0;
+        while(i < p.size()) {
+            if(i + 1 < p.size() && p.get(i) != 1) {
+                sum += p.get(i++) * p.get(i++);
+            } else {
+                sum += p.get(i++);
+            }
+        }
+
+        i = 0;
+        while(i < m.size()) {
+            if (i + 1 < m.size()) {
+                sum += m.get(i++) * m.get(i++);
+            } else {
+                sum += m.get(i++);
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
