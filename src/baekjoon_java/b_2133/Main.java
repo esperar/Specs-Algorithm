8f1a07e72c8cde368d252e1b7d66de2833ec55a9
aesperer <piyrw9754@gmail.com>
2024-07-10 11:37:09 +0900

    2133 타일 채우기 3

---
+++ src/baekjoon_java/b_2133/Main.java
@@ -0,0 +1,40 @@
+package baekjoon_java.b_2133;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int dp(int n) {
+        int[] dp = new int[100];
+
+        if(n == 0) {
+            return 1;
+        }
+
+        if(n == 1) {
+            return 0;
+        }
+
+        if(n == 2) {
+            return 3;
+        }
+
+        if(dp[n] != 0) {
+            return dp[n];
+        }
+
+        int result = 3 * dp(n - 2);
+        for(int i = 3; i <= n; i++) {
+            if(i % 2 == 0) {
+                result += 2 * dp(n - i);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(dp(n));
+    }
+}
