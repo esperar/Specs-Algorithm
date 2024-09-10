b51dfd48e6e3e0fb6efc55211182c61726aec357
aesperer <piyrw9754@gmail.com>
2024-09-10 23:24:52 +0900

    binary search

---
+++ src/study_note/binary_search/Main.kt
@@ -0,0 +1,20 @@
+package study_note.binary_search
+
+fun main() {
+    val arr = intArrayOf(1, 2, 3, 4, 5)
+    println(arr.binarySearch(3))
+}
+
+fun IntArray.binarySearch(value: Int): Int {
+    var start = 0;
+    var last = this.lastIndex
+    val mid = (start + last) / 2
+    while(start <= last) {
+        when {
+            this[mid] > value -> start = mid + 1
+            this[mid] < value -> last = mid - 1
+            this[mid] == value -> return mid
+        }
+    }
+    return - 1
+}
