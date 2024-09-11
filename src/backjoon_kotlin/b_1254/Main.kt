918634536755c7b16c4a2f12e9c408413f55257d
aesperer <piyrw9754@gmail.com>
2024-09-11 11:00:55 +0900

    1254 펠린드롬 만들기

---
+++ src/backjoon_kotlin/b_1254/Main.kt
@@ -0,0 +1,25 @@
+package backjoon_kotlin.b_1254
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val s = readLine()
+    var answer = s.length
+
+    for(i in s.indices) {
+        if(isPallin(s.substring(i))) {
+            break
+        }
+        answer++
+    }
+    println(answer)
+}
+
+private fun isPallin(s: String): Boolean {
+    var start = 0
+    var end = s.length - 1
+    while(start <= end) {
+        if(s[start] != s[end]) return false
+        start++
+        end--
+    }
+    return true
+}
